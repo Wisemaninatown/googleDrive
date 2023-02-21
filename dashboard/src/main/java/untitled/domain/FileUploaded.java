5f9b92de33db43fc8ede081707d33d66adad3f74
wisemaninatown <107032946+Wisemaninatown@users.noreply.github.com>
2023-02-21 13:18:59 +0900

    commit

---
+++ dashboard/src/main/java/untitled/domain/FileUploaded.java
@@ -0,0 +1,12 @@
+package untitled.domain;
+
+import untitled.infra.AbstractEvent;
+import lombok.Data;
+import java.util.*;
+
+
+@Data
+public class FileUploaded extends AbstractEvent {
+
+    private Long id;
+}
